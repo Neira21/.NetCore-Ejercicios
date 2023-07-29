81759275c483dab69f5a6d4a6b0dd4fd72f34ece
Alvaro Neira <alvaroneira98@gmail.com>
2023-07-28 20:50:19 -0500

    Ejercicio validación par o impar

---
+++ validarParImpar/Program.cs
@@ -0,0 +1,22 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        int numero = 0;
+        bool esNumero = false;
+        Console.WriteLine("Ingrese un número");
+        while(!esNumero){
+            esNumero = int.TryParse(Console.ReadLine(), out numero);
+        }
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("El número " + numero + " es par");
+        }
+        else
+        {
+            Console.WriteLine("El número " + numero + " es impar");
+        }
+    }
+}
