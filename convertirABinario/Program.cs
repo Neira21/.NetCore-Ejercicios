93e08d1a0f9c1393331f0455ba5756393e4a66d3
Alvaro Neira <alvaroneira98@gmail.com>
2023-07-28 20:53:22 -0500

    Ejercicio convertir a número binario

---
+++ convertirABinario/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int validarNumero(){
+    int numero = 0;
+    bool esNumero = false;
+    Console.WriteLine("Ingrese un número");
+    while(!esNumero){
+        esNumero = int.TryParse(Console.ReadLine(), out numero);
+        if(!esNumero){
+            Console.WriteLine("Ingrese un número válido");
+        }
+    }
+    return numero;
+}
+
+int numero = validarNumero();
+
+string convertirABinario(int numero){
+    string numeroBinario=string.Empty;
+    
+    while(numero > 0){
+        numeroBinario = (numero % 2).ToString() + numeroBinario;
+        numero = numero / 2;
+        
+    }
+    
+    return numeroBinario;
+}
+
+Console.WriteLine("El número " + numero + " en binario es " + convertirABinario(numero));
+
+
