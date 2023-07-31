aef9946ac71902167d7048b2c2679cd635c44c61
Alvaro Neira <alvaroneira98@gmail.com>
2023-07-31 09:28:38 -0500

    asynawaittask

---
+++ task asyn await/Program.cs
@@ -0,0 +1,21 @@
+﻿
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var task = new Task(()=>{
+            Thread.Sleep(1000);
+            Console.WriteLine("Tarea task interna1");
+        });
+        var task2 = new Task(()=>{
+            Thread.Sleep(1000);
+            Console.WriteLine("Tarea task interna2");
+        });
+        task.Start();
+        task2.Start();
+        await task;
+        await task2;
+        Console.WriteLine("Tarea principal");
+
+    }
+}
